f1e1e8d6d040e70e9e3e2da4c64fdb2160626da2
EwanL <ewan@funkymatic.net>
2022-05-05 09:57:46 +1000

    convert number to string

---
+++ 8kyu/Convert a Number to a String.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/javascript
+// 8 kyu
+// Convert a Number to a String!
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
+// some 8kyu are way easier than others
