ad559662d52f8cf81ef10a01f32d8318d1836b6b
EwanL <ewan@funkymatic.net>
2022-06-30 14:37:47 +1000

    Reversed Strings

---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+// 8 kyu
+// Reversed Strings
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  
+}
+
+// Attempt 1
+function solution(str){
+  return str.split("").reverse().join("")
+}
