00fabdc1c26ee9c3b05a0e4bf3761b1a851a2615
teranian <ewan@funkymatic.net>
2022-07-07 10:20:56 +1000

    7kyu Find the middle element

---
+++ 7kyu/Find the middle element.js
@@ -0,0 +1,53 @@
+// # 7 kyu Find the middle element
+
+// https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme (triplet) {
+
+}
+
+// ## Working
+
+// I will need to sort the array to find the middle value, then return the index number that matches in the original array
+
+// Doesn't work. It returns '1' every time. 
+function gimme (triplet) {
+  const sorted = triplet.sort((a,b) => a-b);
+  console.log(sorted)
+  return triplet.indexOf(sorted[1]);
+}
+
+// Adding square brackets and ... around the array.sort works. 
+function gimme (triplet) {
+  const sorted = [...triplet].sort((a,b) => a-b);
+  console.log(sorted)
+  return triplet.indexOf(sorted[1]);
+}
+
+// ## Solutions
+// The solutions to this problem are wildly different. '
+
+// #1 solution is 
+// function gimme(a) {
+//   return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+// }
+
+// #2 Solution:
+// const gimme = function (arr) {
+//   return arr.indexOf([...arr].sort((x, y) => x > y)[1]);
+// };
+
