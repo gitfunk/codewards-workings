27b32aab0133a429c3d4f4ff5331dad064ce1a69
teranian <ewan@funkymatic.net>
2022-07-12 09:10:49 +1000

    8kyu Even or Odd

---
+++ Codewars/8kyu/Even or Odd.js
@@ -0,0 +1,18 @@
+// # 8 kyu Even or Odd
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  
+}
+
+// ## Working
+
+function even_or_odd(number) {
+  return (number%2 == 0) ? "Even" : "Odd";
+}
+
+// Nailed it first go
