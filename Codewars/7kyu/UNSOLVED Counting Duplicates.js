09d7372a2834b65ccb3721c9d18ca92b41668a69
EwanL <ewan@funkymatic.net>
2022-08-28 23:38:18 +1000

    unsolved counting duplicates. I feel like i'm close.

---
+++ Codewars/7kyu/UNSOLVED Counting Duplicates.js
@@ -0,0 +1,52 @@
+// # 6 kyu Counting Duplicates
+
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/javascript
+
+
+// ## Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// ## Example
+// ```
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+// ```
+
+// ## Solution
+// ```
+// function duplicateCount(text){
+//   //...
+// }
+// ```
+
+
+// ## My Working
+
+// I should start by coverting the string to lowercase. 
+// Split it into an array of separate characters.
+// Loop though and tally each occurance of vowels. 
+
+// ```
+function duplicateCount(text){
+  let duplicates = 0;
+  let newArray = text.toLowerCase().split('');
+  console.log(newArray);
+
+ for (let i = 0; i < newArray.length; i++){
+    console.log(newArray[i]);
+    if (newArray.includes(newArray[i], i+1)) {
+      duplicates++;
+      console.log(duplicates)
+    }
+ };
+ return duplicates;
+}
+// Not working. Works when there are only two instances but just keeps adding for all subsequent matches. 
+// ```
+
+
