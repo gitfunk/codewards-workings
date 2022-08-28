b71627214326072edd80868fc01ada3f5d42e8ca
EwanL <ewan@funkymatic.net>
2022-08-28 23:45:01 +1000

    unsolved Counting Duplicates.

--- Codewars/7kyu/UNSOLVED Counting Duplicates.js
+++ Codewars/7kyu/UNSOLVED Counting Duplicates.js
@@ -65,3 +65,17 @@ function duplicateCount(text){
   
 }
 // also doesn't work.  
+
+
+// ## Unlocked Solutions
+// Solution #1
+function duplicateCount(text){
+  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+
+// Solution #2
+function duplicateCount(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
