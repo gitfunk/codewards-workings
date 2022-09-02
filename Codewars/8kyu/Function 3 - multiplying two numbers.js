d92d90869eed3df51726599a25e19964803c8495
EwanL <ewan@funkymatic.net>
2022-09-02 16:38:58 +1000

    8kyu Multiplying two numbers

---
+++ Codewars/8kyu/Function 3 - multiplying two numbers.js
@@ -0,0 +1,14 @@
+// # 8 kyu Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+// # INSTRUCTIONS
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+// Write here your multiply-function
+
+// # Working
+let multiply = (a, b) => a * b
